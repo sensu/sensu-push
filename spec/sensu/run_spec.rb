44f960c7eb83c2e9fc256f7c2083f9587ec560c3
Sean Porter <portertech@gmail.com>
2020-01-10 09:47:23 -0800

    a working test
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- spec/sensu/run_spec.rb
+++ spec/sensu/run_spec.rb
@@ -3,7 +3,12 @@ RSpec.describe Sensu::Run do
     expect(Sensu::Run::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "can parse cli arguments" do
+    options = Sensu::Run.read_cli_opts(["-c", "foo", "--", "true"])
+    expected = {
+      :check_name => "foo",
+      :command => "true"
+    }
+    expect(options).to eq(expected)
   end
 end
