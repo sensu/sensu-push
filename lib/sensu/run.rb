65ce35b19f3343496136951629961b2a91dc7e0f
Sean Porter <portertech@gmail.com>
2020-01-09 17:18:48 -0800

    base set of cli options
    Signed-off-by: Sean Porter <portertech@gmail.com>

--- lib/sensu/run.rb
+++ lib/sensu/run.rb
@@ -15,6 +15,27 @@ module Sensu
           puts VERSION
           exit
         end
+        opts.on("-c", "--check-name NAME", "Sensu check name") do |name|
+          options[:check_name] = name
+        end
+        opts.on("-e", "--entity-name NAME", "Sensu entity name") do |name|
+          options[:entity_name] = name
+        end
+        opts.on("-t", "--check-timeout SECONDS", "Sensu check execution timeout") do |timeout|
+          options[:timeout] = timeout.to_i
+        end
+        opts.on("-T", "--check-ttl SECONDS", "Sensu check TTL") do |ttl|
+          options[:ttl] = ttl.to_i
+        end
+        opts.on("-H", "--handlers NAME[,NAME]", "Sensu event handlers") do |handlers|
+          options[:handlers] = handlers.split(",")
+        end
+        opts.on("-b", "--backends URL[,URL]", "URL or comma-delimited list of Sensu Go Backend API URLss") do |backends|
+          options[:backends] = backends.split(",")
+        end
+        opts.on("-k", "--api-key KEY", "Sensu Go Backend API key") do |key|
+          options[:api_key] = key
+        end
       end
 
       optparse.parse!(arguments)
